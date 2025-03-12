d5521aa1f56158dc81fde1653e25b92481683ca7
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-03-12 18:12:01 +0530

    vARIABLE

---
+++ Variable And DataTtpe/Variable.js
@@ -0,0 +1,23 @@
+x = 5;
+
+let y = 6;
+y = 7;
+
+const z = 8;
+
+{
+  var p = 20;
+  console.log(p);
+}
+
+var p = "Ankit";
+
+var A = "joining";
+
+{
+  let c = null;
+  console.log(c);
+}
+let c = 5;
+
+console.log(x, y, z, p, A, c);
